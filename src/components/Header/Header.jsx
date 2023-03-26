64e8bc149cbfcdae1c57bd7a28e3f43da825ac91
ehmunna <emran-hussain.munna-tu-ilmenau.de>
2023-03-26 18:10:17 +0200

    header-section-implemented

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import './Header.css';
+import logo from '../../assets/images/Logo.svg';
+const Header = () => {
+  return (
+    <header>
+      {/* menu bar structure */}
+
+      <nav>
+        <a href="">
+          <img src={logo} alt="" srcset="" />
+        </a>
+        {/* list items */}
+        <ul>
+          <li>
+            <a href="http://">Order</a>
+          </li>
+          <li>
+            <a href="http://">Order Review</a>
+          </li>
+          <li>
+            <a href="http://">Manage Inventory</a>
+          </li>
+          <li>
+            <a href="http://">Login</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
