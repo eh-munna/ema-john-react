c693111fb11c9d3ccfaeb72237406f9805da490c
ehmunna <emran-hussain.munna-tu-ilmenau.de>
2023-03-27 00:56:55 +0200

    button position fixed

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -1,15 +1,33 @@
 import React from 'react';
 import './Product.css';
 const Product = (props) => {
-  const { img, name, price, quantity, ratingsCount, seller, shipping, stock } =
-    props.product;
+  const {
+    img,
+    name,
+    price,
+    quantity,
+    ratingsCount,
+    seller,
+    shipping,
+    stock,
+    ratings,
+  } = props.product;
 
   return (
     <div>
       <div className="single-product">
-        <img src={img} alt="" />
-        <h6>{name}</h6>
-        <p>Price : ${price}</p>
+        <div className="product-highlight">
+          <img src={img} alt="" />
+          <h5>{name}</h5>
+        </div>
+        <div className="product-info">
+          <p>Price : ${price}</p>
+          <p>Manufacturer : {seller}</p>
+          <p>Rating : {ratings} Stars</p>
+        </div>
+        <div className="cart-btn ">
+          <button className="w-full">Add to cart</button>
+        </div>
       </div>
     </div>
   );
