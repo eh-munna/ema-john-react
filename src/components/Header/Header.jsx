f5ad0009e65917e08ab9a4f848aa9c48e290a166
ehmunna <emran-hussain.munna-tu-ilmenau.de>
2023-03-26 22:57:28 +0200

    shop component file location fixed

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import './Header.css';
 import logo from '../../assets/images/Logo.svg';
+
+// Header Component
 const Header = () => {
   return (
     <header>
---
+++ src/components/Shop/Shop.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import './Shop.css';
+
+const Shop = () => {
+  return (
+    <section className="shop">
+      <div className="container">
+        <div className="shop-container">
+          <div className="products-container">
+            <h4>Hello</h4>
+          </div>
+          <div className="cart-container">
+            <h5>Order Summery</h5>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Shop;
